23f8c42fba1334ef63b0e9a13bc4219e6fd93999
monik43 <monimons666@gmail.com>
2021-08-27 12:49:51 +0200

    añadido location_id

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -74,4 +74,4 @@ class ImportFile(models.TransientModel):
             s = str(values.get("lot_id"))
             lot_id = s.rstrip('0').rstrip('.') if '.' in s else s
 
-            self.product.update({'move_line_ids':[(0,0, {'lot_id': lot_id, 'qty_done':1, 'product_uom_id':1})]})
+            self.product.update({'move_line_ids':[(0,0, {'lot_id': lot_id, 'qty_done':1, 'product_uom_id':1,'location_id':self.product.location_id})]})
