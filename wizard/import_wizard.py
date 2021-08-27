e36d9bb2b93e140486a3547db1091517af5022fb
monik43 <monimons666@gmail.com>
2021-08-27 10:47:05 +0200

    test

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -36,6 +36,8 @@ class ImportFile(models.TransientModel):
         stock_picking = self.env['stock.picking'].browse(self._context.get('active_ids',[]))
         for product in stock_picking.move_lines:
             print(product.product_id.name, "/"*50)
+            for line in product.move_nosuggest_ids:
+                print(line.CSN)
         return res
 
     @api.multi
