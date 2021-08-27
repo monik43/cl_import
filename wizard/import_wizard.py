a01bf4905943a6f2d7952180f2714afa083e8038
monik43 <monimons666@gmail.com>
2021-08-27 11:03:24 +0200

    test

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -27,7 +27,9 @@ except ImportError:
 
 class ImportFile(models.TransientModel):
     _name = "cl.import.file"
-
+    
+    products_pick = fields.One2many('stock.picking','move_lines')
+    product = fields.Many2one('stock.move')
     file_import = fields.Binary(string="Archivo a importar")
 
     @api.model
