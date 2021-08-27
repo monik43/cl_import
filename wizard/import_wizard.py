9143c5924ac8d50bebe2a851bd09b434afef013d
monik43 <monimons666@gmail.com>
2021-08-27 10:19:25 +0200

    prueba move_lines print

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -28,7 +28,7 @@ except ImportError:
 class ImportFile(models.TransientModel):
     _name = "cl.import.file"
 
-    move_lines = fields.One2many('stock.move')
+    move_lines = fields.One2many('stock.move','picking_id')
     file_import = fields.Binary(string="Archivo a importar")
 
     @api.onchange('move_lines')
