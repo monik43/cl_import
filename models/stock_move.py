5052ecdf4c25425e8f91f669f633e732ce2b3f72
monik43 <monimons666@gmail.com>
2021-08-27 11:48:51 +0200

    name_get stock move

---
+++ models/stock_move.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class stockmove(models.Model):
+    _inherit = 'stock.move'
+
+    def name_get(self):
+        res = []
+        for rec in self:
+            name = rec.product_id.name + ", ["+rec.id+"]"
+            res.append((rec.id,name))
+        return res
