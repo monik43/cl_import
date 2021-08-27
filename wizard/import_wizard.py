55d8ea2cb3a42c90d84b8b7ec3aa688ae7ca0d1f
monik43 <monimons666@gmail.com>
2021-08-27 09:24:12 +0200

    test

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -72,7 +72,7 @@ class ImportFile(models.TransientModel):
                     for val in values:
                         print(val)
                         for v in val:
-                            print(v,": ", val[v])
+                            print(v)
                     #res = self.create_chart_accounts(values)
         else:
             raise Warning(_("Formato incorrecto"))
