ae744add59a15477041ac8649e3264ef7e4d24b0
monik43 <monimons666@gmail.com>
2021-08-30 10:32:06 +0200

    test move lines

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -29,7 +29,7 @@ class ImportFile(models.TransientModel):
     _name = "cl.import.file"
     
     products_move = fields.One2many('stock.move','picking_id')
-    product = fields.Many2one('stock.move', "Producto a procesar", domain="[('name','in',products_move)]")
+    product = fields.Many2one('stock.move', "Producto a procesar", domain="[('product_id','in',products_move.product_id)]")
     file_import = fields.Binary("Archivo a importar")
 
     
