8c35d5f5d60fd798e5a144011963055bd6fdfd89
monik43 <monimons666@gmail.com>
2021-08-30 10:50:38 +0200

    test move lines

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -39,7 +39,7 @@ class ImportFile(models.TransientModel):
     def default_get(self, fields):
         res = super(ImportFile, self).default_get(fields)
         stock_picking = self.env['stock.picking'].browse(self._context.get('active_ids',[]))
-        print(stock_picking.origin)
+        res.update({'origin': stock_picking.origin})
         for line in stock_picking.move_lines:
             res.update({'products_move': [(0,0, {'product_uom_id':1, 'date': line.date, 'date_expected': line.date_expected,'id': line.id, 'name':line.name, 'product_id':line.product_id.id, 'product_uom_qty': line.product_uom_qty, 'location_id': line.location_id.id, 'location_dest_id': line.location_dest_id.id})]})
         return res
