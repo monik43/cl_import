d4b6d9584ba4c2c2ee072ace946d75a27b7ce248
monik43 <monimons666@gmail.com>
2021-08-30 10:52:50 +0200

    test move lines

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -70,4 +70,4 @@ class ImportFile(models.TransientModel):
         if values.get("lot_id"):
             s = str(values.get("lot_id"))
             lot_id = s.rstrip('0').rstrip('.') if '.' in s else s
-            res.create({'move_line_nosuggest_ids':[(0,0, {'name':str(lot_id),'lot_id': lot_id, 'qty_done':1, 'product_uom_id':1,'location_id':self.product.location_id,'location_dest_id':self.product.location_dest_id})]})
+            res.update({'move_line_nosuggest_ids':[(0,0, {'name':str(lot_id),'lot_id': lot_id, 'qty_done':1, 'product_uom_id':1,'location_id':self.product.location_id,'location_dest_id':self.product.location_dest_id})]})
