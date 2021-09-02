9a08667abd32cbc4fe32856d91ef95a0f3c3754d
monik43 <monimons666@gmail.com>
2021-09-02 10:21:18 +0200

    test

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -76,6 +76,7 @@ class ImportFile(models.TransientModel):
             for line in res.move_lines:
                 if line.product_id.id == self.product.id:
                     data["id"] = line.product_id.id
+                    print(line.name, "/"*25)
                     data["name"] = line.name
 
             res.update({'move_lines': [(1, data.get("id"), {'name':data.get("name"),'move_line_nosuggest_ids': [(0, 0, {'lot_id': lot_id, 'qty_done': 1,'product_uom_id': 1, 'location_id': self.product.location_id, 'location_dest_id': self.product.location_dest_id})]})]})
