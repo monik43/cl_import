1b30df5afb8ed0e1f0c05eb2f8f0d64b7f8e5092
monik43 <monimons666@gmail.com>
2021-09-02 10:44:10 +0200

    test

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -76,7 +76,7 @@ class ImportFile(models.TransientModel):
             for line in res.move_lines:
                 print(self.product.product_id.id, " - ", self.product.product_id)
                 if line.product_id.id == self.product.product_id.id:
-                    data["id"] = line.product_id.product_id.id
+                    data["id"] = line.product_id.id
                     data["name"] = line.name
 
             res.update({'move_lines': [(1, data.get("id"), {'name':data.get("name"),'move_line_nosuggest_ids': [(0, 0, {'lot_id': lot_id, 'qty_done': 1,'product_uom_id': 1, 'location_id': self.product.location_id, 'location_dest_id': self.product.location_dest_id})]})]})
