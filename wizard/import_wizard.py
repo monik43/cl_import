e82984007e76cf1266e7e0d5ca72e7227776cee4
monik43 <monimons666@gmail.com>
2021-09-02 10:02:16 +0200

    test

--- wizard/import_wizard.py
+++ wizard/import_wizard.py
@@ -79,5 +79,5 @@ class ImportFile(models.TransientModel):
                     u_id = line.product_id.id
                     g_id = line.group_id.id
 
-            res.update({'move_lines': [(1, u_id, {'move_line_nosuggest_ids': [(0, 0, {'lot_id': lot_id, 'qty_done': 1,'product_uom_id': 1, 'location_id': self.product.location_id, 'location_dest_id': self.product.location_dest_id, 'group_id': g_id})]})]})
+            res.update({'move_lines': [(1, u_id, {'move_line_nosuggest_ids': [(0, 0, {'name':"uhfgaehgbaegaielrgae",'lot_id': lot_id, 'qty_done': 1,'product_uom_id': 1, 'location_id': self.product.location_id, 'location_dest_id': self.product.location_dest_id, 'group_id': g_id})]})]})
         return res
